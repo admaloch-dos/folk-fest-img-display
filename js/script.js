5c20045770e2e0688cd8da72a8860372a9670e95
davis maloch (fl. memory) <133890108+admaloch-dos@users.noreply.github.com>
2023-05-24 11:18:11 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,234 @@
+const images = ["001", "002", "003", "004", "005", "006", "007", "008", "009", "010", "011", "012", "013", "014", "015", "016", "017", "018", "019", "020", "021", "022", "023", "024", "025", "026", "027", "028", "029", "030", "031", "032", "033", "034", "035", "036", "037", "038", "039", "040", "041", "042", "043", "044", "045", "046", "047", "048", "049", "050", "051", "052", "053", "054", "055", "056", "057", "058", "059", "060", "061", "062", "063", "064", "065", "066", "067", "068", "069", "070", "071", "072", "073", "074", "075", "076", "077"]
+
+$('.carousel').carousel({
+
+
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  mobileFirst: true
+});
+
+// function to generate random number that doesn't repeat
+let nums = [];
+const genRandNum = (max) => {
+  let rand = (Math.random() * max).toFixed();
+  rand = Number(rand);
+  if (!nums.includes(rand)) {
+    nums.push(rand);
+    return rand;
+  } else {
+    if (nums.length < max) {
+      return genRandNum(max);
+    } else {
+      console.log('No more numbers available.')
+      return false;
+    }
+  }
+}
+
+
+const carouselInner = document.querySelector('.carousel-inner')
+
+for (let i = 0; i < images.length - 1; i++) {
+  let randIndex = genRandNum(images.length - 1)
+  let randImage = images[randIndex]
+  const newItem = document.createElement('div')
+  newItem.id = randIndex
+  newItem.classList.add('carousel-item')
+  let newImg = document.createElement('img')
+  newImg.src = `img/image${randImage}.jpg`
+  newImg.classList.add('d-block', 'mx-auto')
+  newItem.append(newImg)
+  carouselInner.append(newItem)
+}
+const firstItem = document.querySelector('.carousel-item').classList.add('active')
+
+
+
+
+
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+const currTime = document.querySelector('#currTime');
+const durTime = document.querySelector('#durTime');
+
+// Song titles
+const songs = ['01 Vive y Vacila', '02 Agua Que Va Caer', '03 Ochosí', '04 Manteca', '05 Drum Instrumental', '06 Bomba Demonstration', '07 Eleguá (El Niño de Atocha)', '08 Guantanamera', '09 Yo Canto en el Llano', '10 Yo Llegaré', '11 Afro-Samba', '12 Pasaje', '13 Alma, Corazón y Vida', '14 El Cóndor Pasa', '15 Embrujo', '16 Guadalajara', '17 Una Morenita', '18 Vuela Paloma', '19 Tomando Licores', '20 Cielito Lindo'];
+
+// Keep track of song
+let songIndex = 0;
+
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+// Update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+
+}
+
+// Play song
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
+}
+
+// Pause song
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause();
+}
+
+// Previous song
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// Next song
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// Update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+//get duration & currentTime for Time of song
+function DurTime(e) {
+  const { duration, currentTime } = e.srcElement;
+  var sec;
+  var sec_d;
+
+  // define minutes currentTime
+  let min = (currentTime == null) ? 0 :
+    Math.floor(currentTime / 60);
+  min = min < 10 ? '0' + min : min;
+
+  // define seconds currentTime
+  function get_sec(x) {
+    if (Math.floor(x) >= 60) {
+
+      for (var i = 1; i <= 60; i++) {
+        if (Math.floor(x) >= (60 * i) && Math.floor(x) < (60 * (i + 1))) {
+          sec = Math.floor(x) - (60 * i);
+          sec = sec < 10 ? '0' + sec : sec;
+        }
+      }
+    } else {
+      sec = Math.floor(x);
+      sec = sec < 10 ? '0' + sec : sec;
+    }
+  }
+
+  get_sec(currentTime, sec);
+
+  // change currentTime DOM
+  currTime.innerHTML = min + ':' + sec;
+
+  // define minutes duration
+  let min_d = (isNaN(duration) === true) ? '0' :
+    Math.floor(duration / 60);
+  min_d = min_d < 10 ? '0' + min_d : min_d;
+
+
+  function get_sec_d(x) {
+    if (Math.floor(x) >= 60) {
+
+      for (var i = 1; i <= 60; i++) {
+        if (Math.floor(x) >= (60 * i) && Math.floor(x) < (60 * (i + 1))) {
+          sec_d = Math.floor(x) - (60 * i);
+          sec_d = sec_d < 10 ? '0' + sec_d : sec_d;
+        }
+      }
+    } else {
+      sec_d = (isNaN(duration) === true) ? '0' :
+        Math.floor(x);
+      sec_d = sec_d < 10 ? '0' + sec_d : sec_d;
+    }
+  }
+
+  // define seconds duration
+
+  get_sec_d(duration);
+
+  // change duration DOM
+  durTime.innerHTML = min_d + ':' + sec_d;
+
+};
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
+
+// Time of song
+audio.addEventListener('timeupdate', DurTime);
+
+const audioTracks = document.querySelector('#audio')
+
+
